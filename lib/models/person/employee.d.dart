9f3e8a9bbda7b26339dc5445f401ecd945233843
VinhPNA <vinhpna@1cinnovation.com>
2023-08-21 18:17:39 +0700

    add attendance page

--- lib/models/person/employee.d.dart
+++ lib/models/person/employee.d.dart
@@ -1,5 +1,8 @@
 import 'dart:math';
 
+import 'package:hanet/models/department/department.d.dart';
+import 'package:hanet/models/person/person.d.dart';
+
 class Employee {
   String? profile;
   String? name;
@@ -30,5 +33,27 @@ class EmployeeBuilder {
         department: random.nextInt(2) % 2 == 0 ? "IT" : "Accounting",
         postion: positions[random.nextInt(positions.length)],
         categorize: "Employee");
+  }
+
+  static Employee fromPersonAndDepartment(
+      HanetPerson person, HanetDepartment department) {
+    Employee employee = Employee();
+    // assign data
+    employee.name = person.name;
+    switch (person.type) {
+      case 0:
+        employee.categorize = "Staff";
+        break;
+      case 1:
+        employee.categorize = "Employee";
+        break;
+      default:
+        employee.categorize = "Other";
+    }
+    employee.profile = person.avatar;
+    employee.postion = person.title;
+    employee.department = department.name;
+
+    return employee;
   }
 }
--- lib/pages/employee/page.dart
+++ lib/pages/employee/page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:hanet/components/SearchButton.dart';
 import 'package:hanet/controllers/person/person.ctrl.dart';
+import 'package:hanet/controllers/place/place.ctrl.dart';
 import 'package:hanet/layout/app_layout.dart';
 import 'package:hanet/layout/data_list_layout.dart';
 import 'package:hanet/models/constants/styles.c.dart';
@@ -11,19 +12,15 @@ import 'package:syncfusion_flutter_datagrid/datagrid.dart';
 import '../../models/person/employee.d.dart';
 import '../../models/person/employee_data_source.dart';
 
-class EmployeeScreen extends StatelessWidget {
+class EmployeeScreen extends StatefulWidget {
   EmployeeScreen({super.key});
 
-  static const TextStyle columnTitle = TextStyle(
-      color: Color(0xFF64748B), fontSize: 15, fontWeight: FontWeight.w600);
+  @override
+  State<EmployeeScreen> createState() => _EmployeeScreenState();
+}
 
-  final List<Employee> employees = [
-    EmployeeBuilder.createAnEmployee(),
-    EmployeeBuilder.createAnEmployee(),
-    EmployeeBuilder.createAnEmployee(),
-    EmployeeBuilder.createAnEmployee(),
-    EmployeeBuilder.createAnEmployee(),
-  ];
+class _EmployeeScreenState extends State<EmployeeScreen> {
+  RxList<Employee> employees = <Employee>[].obs;
 
   final List<String> places = [
     "Ha Noi",
@@ -31,13 +28,25 @@ class EmployeeScreen extends StatelessWidget {
     "Ho Chi Minh City",
   ];
 
+  @override
+  void initState() {
+    super.initState();
+    final personCtrl = Get.find<PersonController>();
+    final placeCtrl = Get.find<PlaceController>();
+    String placeID = placeCtrl.places[0].id.toString();
+    employees.value = EmployeeDataSource.convertListPersonInPlace(
+        personCtrl.peopleMap[placeID] ?? []);
+    if (placeCtrl.places.isNotEmpty) {
+      personCtrl.getPeopleByPlace(placeID).then((people) {
+        employees.value = EmployeeDataSource.convertListPersonInPlace(people);
+      });
+    }
+  }
+
   Rx<String?> selectedPlace = Rx(null);
 
   @override
   Widget build(BuildContext context) {
-    final personCtrl = Get.find<PersonController>();
-    personCtrl.getPeopleByPlace("18265");
-    final employeeSource = EmployeeDataSource(employees: employees);
     return AppLayout(
       child: SingleChildScrollView(
           child: DataListLayout(
@@ -89,68 +98,79 @@ class EmployeeScreen extends StatelessWidget {
                   Row(
                     children: [
                       Expanded(
-                        child: SfDataGrid(source: employeeSource, columns: [
-                          GridColumn(
-                            width: 80,
-                            columnName: "profile",
-                            label: Container(
-                              color: Colors.white,
-                              alignment: Alignment.center,
-                              child: const Text(
-                                "Profile",
-                                style: columnTitle,
-                              ),
-                            ),
-                          ),
-                          GridColumn(
-                              columnWidthMode: ColumnWidthMode.fill,
-                              columnName: "name",
-                              minimumWidth: 150,
-                              allowSorting: true,
-                              label: Container(
-                                  color: Colors.white,
-                                  alignment: Alignment.centerLeft,
-                                  child: Text("Name", style: columnTitle))),
-                          GridColumn(
-                              columnWidthMode: ColumnWidthMode.fitByCellValue,
-                              allowSorting: true,
-                              minimumWidth: 100,
-                              columnName: "postion",
-                              label: Container(
-                                  color: Colors.white,
-                                  alignment: Alignment.centerLeft,
-                                  child: const Text("Postion",
-                                      style: columnTitle))),
-                          GridColumn(
-                              columnName: "categorize",
-                              minimumWidth: 150,
-                              columnWidthMode: ColumnWidthMode.fill,
-                              allowSorting: true,
-                              label: Container(
-                                  color: Colors.white,
-                                  alignment: Alignment.centerLeft,
-                                  child:
-                                      Text("Categorize", style: columnTitle))),
-                          GridColumn(
-                              columnName: "department",
-                              columnWidthMode: ColumnWidthMode.fill,
-                              allowSorting: true,
-                              minimumWidth: 200,
-                              label: Container(
-                                  color: Colors.white,
-                                  alignment: Alignment.centerLeft,
-                                  child:
-                                      Text("Department", style: columnTitle))),
-                          GridColumn(
-                            width: 100,
-                            columnName: "actions",
-                            label: Container(
-                              color: Colors.white,
-                              alignment: Alignment.center,
-                              child: Text("Actions", style: columnTitle),
-                            ),
-                          ),
-                        ]),
+                        child: Obx(
+                          () => SfDataGrid(
+                              source: EmployeeDataSource(employees: employees),
+                              columns: [
+                                GridColumn(
+                                  width: 80,
+                                  columnName: "profile",
+                                  label: Container(
+                                    color: Colors.white,
+                                    alignment: Alignment.center,
+                                    child: const Text(
+                                      "Profile",
+                                      style: HanetTextStyles.columnTitle,
+                                    ),
+                                  ),
+                                ),
+                                GridColumn(
+                                    columnWidthMode: ColumnWidthMode.fill,
+                                    columnName: "name",
+                                    minimumWidth: 150,
+                                    allowSorting: true,
+                                    label: Container(
+                                        color: Colors.white,
+                                        alignment: Alignment.centerLeft,
+                                        child: Text("Name",
+                                            style:
+                                                HanetTextStyles.columnTitle))),
+                                GridColumn(
+                                    columnWidthMode:
+                                        ColumnWidthMode.fitByCellValue,
+                                    allowSorting: true,
+                                    minimumWidth: 100,
+                                    columnName: "postion",
+                                    label: Container(
+                                        color: Colors.white,
+                                        alignment: Alignment.centerLeft,
+                                        child: const Text("Postion",
+                                            style:
+                                                HanetTextStyles.columnTitle))),
+                                GridColumn(
+                                    columnName: "categorize",
+                                    minimumWidth: 150,
+                                    columnWidthMode: ColumnWidthMode.fill,
+                                    allowSorting: true,
+                                    label: Container(
+                                        color: Colors.white,
+                                        alignment: Alignment.centerLeft,
+                                        child: Text("Categorize",
+                                            style:
+                                                HanetTextStyles.columnTitle))),
+                                GridColumn(
+                                    columnName: "department",
+                                    columnWidthMode: ColumnWidthMode.fill,
+                                    allowSorting: true,
+                                    minimumWidth: 200,
+                                    label: Container(
+                                        color: Colors.white,
+                                        alignment: Alignment.centerLeft,
+                                        child: Text("Department",
+                                            style:
+                                                HanetTextStyles.columnTitle))),
+                                GridColumn(
+                                  width: 100,
+                                  columnName: "actions",
+                                  label: Container(
+                                    color: Colors.white,
+                                    alignment: Alignment.center,
+                                    child: Text("Actions",
+                                        style: HanetTextStyles.columnTitle),
+                                  ),
+                                ),
+                              ]),
+                        ),
                       ),
                     ],
                   ),
