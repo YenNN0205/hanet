16e87b39c77cb17ed324d1a4ddbfb236de379d58
vinhpna1c <vinhpna@1cinnovation.com>
2023-09-18 12:54:42 +0700

    update webconfig + loading page

--- lib/r.dart
+++ lib/r.dart
@@ -7,6 +7,7 @@ class AssetImages {
 	static const String avatar = 'assets/images/avatar.png';
 	static const String hanetLogo = 'assets/images/hanet_logo.png';
 	static const String logo = 'assets/images/logo.png';
+	static const String techfisLogo = 'assets/images/techfis_logo.png';
 }
 
 class AssetAssets {
