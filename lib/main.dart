484cd601067e4cf418bb2157a1b86c3aca1fba18
vinhpna1c <vinhpna@1cinnovation.com>
2023-09-18 13:14:04 +0700

    update name display

--- lib/main.dart
+++ lib/main.dart
@@ -29,7 +29,7 @@ class MyApp extends StatelessWidget {
       ],
       debugShowCheckedModeBanner: false,
       scrollBehavior: MyCustomScrollBehavior(),
-      title: 'Hanet:1C',
+      title: 'TechFis',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
